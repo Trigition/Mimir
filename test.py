47575e2301c84412b1c519f22d3877a9f2f1bd02
BuildTools <trigition@gmail.com>
2015-11-12 14:32:36 -0700

    Added file output

--- test.py
+++ test.py
@@ -13,12 +13,13 @@ Images, Targets = olivetti.data, olivetti.target
 dim = Images.shape
 print "DIMENSION:", dim
 output = open("test.out", "w")
-output.write("Epoch,Error")
+output.write("Epoch,Error\n")
 for i in range(1000):  
   imageData = ImageData(Images, Targets, 64, 64, outputs = 1)
   imageData.train_all_data(0.25)
 
   testNet = Network(imageData, 64, "Softmax")
-  testNet.init_backprop_trainer(verbose=False)
+  testNet.init_backprop_trainer(b_verbose=False)
   error = testNet.run_network(i)
-  output.write(str(i) + "," + str(error))
+  print "Epochs:", i,"Error:", error
+  output.write(str(i) + "," + str(error) + "\n")
